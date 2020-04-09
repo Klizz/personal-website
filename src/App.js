2323e646604a41ca487a82132f5c4beb2ffbb5fb
Klizz <klizzy@hotmail.es>
2020-04-09 15:20:34 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'materialize-css';
+import { Button, Card, Row, Col } from 'react-materialize';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container center-align">
+      <img src="./images/photo.jpg" alt="Karen Ibarra" id="profile-pic"/>
+      <h1 className="brown-text">Hola, soy Karen Ibarra</h1>
     </div>
   );
 }
