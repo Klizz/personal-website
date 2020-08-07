55be1be92fcf41e901032c7437c3d00427b0c6d3
Karen <klizzy@hotmail.es>
2020-08-07 16:48:15 -0500

    update

--- src/pages/es.js
+++ src/pages/es.js
@@ -5,6 +5,7 @@ class En extends Component {
     return (
       <div>
         <section className="hero" id="top">
+          <div className="col s7 push-s5">
           <h1 className="animate__animated animate__fadeInRight">
             Hola, mi nombre es Karen
           </h1>
@@ -15,14 +16,27 @@ class En extends Component {
             href="mailto:klizzy@hotmail.es"
             className="animate__animated animate__fadeIn animate__delay-4s"
           >
-            <button className="scroll">
-              <i class="large material-icons">contact_mail</i>
-            </button>
           </a>
+          </div>
+          <div className="col s5 pull-s7">
+          <a href="mailto:klizzy@hotmail.es">
+            <img src="./icons/correo.svg" alt="mail" className="icon" />
+          </a>
+          <a href="https://github.com/Klizz">
+            <img src="./icons/imagen-de-github.svg" alt="github" className="icon" />
+          </a>
+          <a href="https://www.linkedin.com/in/karen-ibarra-662a94111/">
+            <img src="./icons/linkedin.svg" alt="linkedin" className="icon" />
+          </a>
+          </div>
         </section>
+
+
         <div className="bottom-center">
           <a href="#about">
             <button className="scroll">
+              Ver mas
+              <br />
               <i class="large material-icons">expand_more</i>
             </button>
           </a>
@@ -31,9 +45,8 @@ class En extends Component {
         <section className="fullscreen" id="about">
           <div>
             <h1 className="headline">Acerca de mí</h1>
-            <p>
-              Soy una aspirante a desarrolladora web con poca experiencia pero
-              mucha motivación para aprender.
+            <p className="flow-text">
+              Mi meta es convertirme en desarrolladora web.
               <br />
               Actualmente resido en la ciudad de Monterrey, Nuevo León y tengo
               interés en trabajar en empresas de esa ciudad o de manera remota
@@ -50,9 +63,16 @@ class En extends Component {
 
         <section className="fullscreen" id="stack">
           <h1 className="headline">Tecnologías</h1>
-          <p>
+          <p className="flow-text">
             Estas son algunas de las tecnologías con las que he desarrollado
-            proyectos:
+            proyectos: <br />
+            <img src="./logos/html5.png" alt="html5" className="logos" />
+            <img src="./logos/css3.png" alt="css3" className="logos" />
+            <img src="./logos/javascript.png" alt="javascript" className="logos" />
+            <img src="./logos/python.png" alt="python" className="logos" />
+            <br />
+            <img src="./logos/reactjs.png" alt="reactjs" className="logos" />
+            <img src="./logos/nodejs.png" alt="nodejs" className="logos" />
           </p>
         </section>
       </div>
