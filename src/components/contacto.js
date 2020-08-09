590fb95c0353da27b0df24eb7656e56c879b0e99
Karen <klizzy@hotmail.es>
2020-08-09 16:09:49 -0500

    Added contact component

---
+++ src/components/contacto.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { withStyles } from '@material-ui/core/styles';
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import MailIcon from "@material-ui/icons/Mail";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
+import CloseIcon from '@material-ui/icons/Close';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import MuiDialogActions from '@material-ui/core/DialogActions';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+
+
+const styles = (theme) => ({
+    root: {
+      margin: 0,
+      padding: theme.spacing(2),
+    },
+    closeButton: {
+      position: 'absolute',
+      right: theme.spacing(1),
+      top: theme.spacing(1),
+      color: theme.palette.grey[500],
+    },
+  });
+
+const DialogTitle = withStyles(styles)((props) => {
+    const { children, classes, onClose, ...other } = props;
+    return (
+      <MuiDialogTitle disableTypography className={classes.root} {...other}>
+        <Typography variant="h6">{children}</Typography>
+        {onClose ? (
+          <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </MuiDialogTitle>
+    );
+  });
+
+export default function CustomizedDialogs(){
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    const DialogContent = withStyles((theme) => ({
+        root: {
+          padding: theme.spacing(2),
+        },
+      }))(MuiDialogContent);
+      
+      const DialogActions = withStyles((theme) => ({
+        root: {
+          margin: 0,
+          padding: theme.spacing(1),
+        },
+      }))(MuiDialogActions);
+
+    return (
+      <div className="container centered-content">
+        <h1>¡Contáctame!</h1>
+        <a href="https://github.com/Klizz">
+          <GitHubIcon className="icon" fontSize="large" />
+        </a>
+        <a href="https://www.linkedin.com/in/karen-ibarra-662a94111/">
+          <LinkedInIcon className="icon" fontSize="large" />
+        </a>
+        <a href="mailto:klizzy@hotmail.es">
+          <MailIcon className="icon" fontSize="large" />
+        </a>
+        <WhatsAppIcon className="icon" fontSize="large" onClick={handleClickOpen} />
+
+        <Dialog onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle id="customized-dialog-title" onClose={handleClose}>
+        <span className="highlight-text">WhatsApp</span>
+        </DialogTitle>
+        <DialogContent dividers>
+        <p className="flow-text">
+            Si necesitas contactarme a traves de Whatsapp mi numero es <span className="headline">826-108-45-96</span>
+        </p>
+        </DialogContent>
+      </Dialog>
+
+      </div>
+    );
+}
--- src/pages/es.js
+++ src/pages/es.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import MailIcon from '@material-ui/icons/Mail';
 import Typography from '@material-ui/core/Typography';
+import Rating from '@material-ui/lab/Rating';
+import BuildIcon from '@material-ui/icons/Build';
+import CodeIcon from '@material-ui/icons/Code';
+import LaptopChromebookIcon from '@material-ui/icons/LaptopChromebook';
+import Contacto from '../components/contacto';
+
 
 class Es extends Component {
   render() {
@@ -10,34 +13,26 @@ class Es extends Component {
       <div>
         <section className="hero" id="top">
 
+          <div className="row">
+          <div className="col s12 m8">
           <Typography className="animate__animated animate__fadeInRight" variant="h1" component="h2" gutterBottom>
           Hola, mi nombre es Karen
           </Typography>
           <Typography className="animate__animated animate__fadeInRight animate__delay-2s" variant="h3" component="h2" gutterBottom>
           Soy Web Developer Jr.
           </Typography>
-          <a
-            href="mailto:klizzy@hotmail.es"
-            className="animate__animated animate__fadeIn animate__delay-4s"
-          ></a>
-          <a href="mailto:klizzy@hotmail.es">
-          <MailIcon 
-          className="icon"
-          fontSize="large"
-          />
-          </a>
-          <a href="https://github.com/Klizz">
-          <GitHubIcon 
-          className="icon"
-          fontSize="large"
-          />
-          </a>
-          <a href="https://www.linkedin.com/in/karen-ibarra-662a94111/">
-          <LinkedInIcon 
-          className="icon"
-          fontSize="large"
-          />
-          </a>
+          </div>
+
+          <div className="col s12 m4 animate__animated animate__fadeIn animate__delay-3s">
+            <h4>Navegación</h4>
+            <ul>
+              <a href="#about" className="nav"><li>Sobre mí</li></a>
+              <a href="#stack" className="nav"><li>Tecnologías</li></a>
+              <a href="#contact" className="nav"><li>Contáctame</li></a>
+            </ul>
+          </div>
+          </div>
+          
         </section>
 
         <div className="bottom-center">
@@ -72,24 +67,91 @@ class Es extends Component {
           </div>
           </div>
         </section>
-
-        <section className="fullscreen" id="stack">
+        <div className="space"></div>
+        <section className="container centered-content" id="stack">
           <h1 className="headline">Tecnologías</h1>
-          <p className="flow-text">
-            Estas son algunas de las tecnologías y lenguajes con las que he desarrollado
-            proyectos: <br />
-            <img src="./logos/html5.png" alt="html5" className="logos" />
-            <img src="./logos/css3.png" alt="css3" className="logos" />
-            <img
-              src="./logos/javascript.png"
-              alt="javascript"
-              className="logos"
+          <div className="space"></div>
+          <div className="row">
+          <div className="col s12 m4 centered-content">
+          <CodeIcon 
+            className="icon-color"
+            fontSize="large"
             />
-            <img src="./logos/python.png" alt="python" className="logos" />
-            <br />
-            <img src="./logos/reactjs.png" alt="reactjs" className="logos" />
-            <img src="./logos/nodejs.png" alt="nodejs" className="logos" />
-          </p>
+            <div className="space"></div>
+            <span className="highlight-text">Lenguajes:</span>
+            <ul>
+              <li>HTML5</li>
+              <Rating name="read-only" value={4.5} precision={0.5} readOnly />
+              <div className="space"></div>
+              <li>CSS3</li>
+              <Rating name="read-only" value={4} readOnly />
+              <div className="space"></div>
+              <li>Sass</li>
+              <Rating name="read-only" value={3.5} precision={0.5} readOnly />
+              <div className="space"></div>
+              <li>JavaScript</li>
+              <Rating name="read-only" value={4} readOnly />
+              <div className="space"></div>
+              <li>Python</li>
+              <Rating name="read-only" value={2.5} precision={0.5} readOnly />
+              <div className="space"></div>
+              <li>Ruby</li>
+              <Rating name="read-only" value={2} readOnly />
+              <div className="space"></div>
+            </ul>
+          </div>
+          <div className="col s12 m4 centered-content">
+          <LaptopChromebookIcon 
+            className="icon-color"
+            fontSize="large"
+            />
+            <div className="space"></div>
+            <span className="highlight-text">Frameworks y librerías:</span>
+            <ul>
+              <li>React.js</li>
+              <Rating name="read-only" value={3} readOnly />
+              <div className="space"></div>
+              <li>NodeJS</li>
+              <Rating name="read-only" value={2.5} precision={0.5} readOnly />
+              <div className="space"></div>
+              <li>Material UI</li>
+              <Rating name="read-only" value={2} readOnly />
+              <div className="space"></div>
+              <li>Bootstrap</li>
+              <Rating name="read-only" value={3} readOnly />
+              <div className="space"></div>
+              <li>MaterializeCSS</li>
+              <Rating name="read-only" value={4} readOnly />
+              <div className="space"></div>
+            </ul>
+          </div>
+          <div className="col s12 m4 centered-content">
+            <BuildIcon 
+            className="icon-color"
+            fontSize="large"
+            />
+            <div className="space"></div>
+            <span className="highlight-text">Herramientas:</span>
+            <ul>
+              <li>Git</li>
+              <Rating name="read-only" value={3} readOnly />
+              <div className="space"></div>
+              <li>GitHub</li>
+              <Rating name="read-only" value={2} readOnly />
+              <div className="space"></div>
+              <li>Visual Studio Code</li>
+              <Rating name="read-only" value={3} readOnly />
+              <div className="space"></div>
+              <li>Terminal de Linux</li>
+              <Rating name="read-only" value={4} readOnly />
+              <div className="space"></div>
+            </ul>
+          </div>
+          </div>
+        </section>
+        <div className="space"></div>
+        <section className="contact-section" id="contact">
+          <Contacto />
         </section>
       </div>
     );
